4eaed38ef5d5d011838015061c5b45bc70160b6a
Egor Aralov <aei.omsk@gmail.com>
2017-10-31 00:12:18 +0100

    Add dynamic drop down support

---
+++ Attibutes/DropDownEntry.cs
@@ -0,0 +1,14 @@
+﻿namespace SkyboxReplacer.OptionsFramework.Attibutes
+{
+    public struct DropDownEntry<TKey>
+    {
+        public TKey Code;
+        public string Description;
+
+        public DropDownEntry(TKey code, string description)
+        {
+            Code = code;
+            Description = description;
+        }
+    }
+}
---
+++ Attibutes/DynamicDropDownAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace SkyboxReplacer.OptionsFramework.Attibutes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DynamicDropDownAttribute : DropDownAttribute
+    {
+        public DynamicDropDownAttribute(string description, string populatorClass, string populatorMethod, string group = null, string actionClass = null, string actionMethod = null) :
+            base(description, group, actionClass, actionMethod)
+        {
+            this.Populator = () =>
+            {
+                var method = Util.FindType(populatorClass).GetMethod(populatorMethod, BindingFlags.Public | BindingFlags.Static);
+                return (List<DropDownEntry<string>>)method.Invoke(null, new object[] { });
+            };
+        }
+
+        public IList<DropDownEntry<string>> GetItems(Func<string, string> translator = null)
+        {
+            var entries = Populator.Invoke();
+            return (from DropDownEntry<string> entry in entries
+                    let code = entry.Code
+                    let description = entry.Description
+                    let translatedDesctiption = translator == null ? description : translator.Invoke(description)
+                    select new DropDownEntry<string>(code, translatedDesctiption)).ToList();
+        }
+
+        private Func<List<DropDownEntry<string>>> Populator { get; }
+    }
+}
