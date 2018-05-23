71a298efb765923829c9a35f065ed41bb064bfc3
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2018-05-23 22:29:39 +0200

    Use type safe way of invoking actions

--- Attibutes/DropDownAttribute.cs
+++ Attibutes/DropDownAttribute.cs
@@ -7,7 +7,7 @@ namespace OptionsFramework.Attibutes
     [AttributeUsage(AttributeTargets.Property)]
     public abstract class DropDownAttribute : AbstractOptionsAttribute
     {
-        protected DropDownAttribute(string description, string group, string actionClass, string actionMethod) : base(
+        protected DropDownAttribute(string description, string group, Type actionClass, string actionMethod) : base(
             description, group, actionClass, actionMethod)
         {
         }
